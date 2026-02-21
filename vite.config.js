f36e0819d624d73132fb6b9ca5765e1b7d948489
A <mahongue@yahoo.com>
2026-02-21 15:58:30 -0500

    Configure reliable GitHub Pages deployment for project path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed back from '/EarthScienceRegents/' to '/'
+  base: '/mekambo-site/',
 })
